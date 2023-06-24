98ecac89650ef1ffa6e5eb85228ffe3e49248eec
Oleksandr <sashasashok.sherbyna@gmail.com>
2023-06-24 22:06:24 +0200

    angular material added

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatTabsModule,
+    MatButtonModule
+  ]
+})
+export class MatreialModule { }
